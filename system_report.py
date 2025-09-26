12f33472e03925aa4e9cd115b403de8c6037115a
Alex <Alexfrate12@gmail.com>
2025-09-26 00:04:24 -0400

    third

--- system_report.py
+++ system_report.py
@@ -32,50 +32,69 @@ def network_information():
     print(f"DNS2: \t\t\t\t{ips[1]}")
 
 def os_information():
+    print(f"\033[92mOperating System Information\n\033[00m")
     # OS
     os_info = subprocess.run("cat /etc/os-release",capture_output=True,shell=True).stdout.strip().decode()
-    os_name = re.search(r'^PRETTY_NAME="([^"]+)"', os_info)
+    os_name = re.search(r'^PRETTY_NAME="([^"]+)"', os_info,re.MULTILINE)
     print(f"Operating System: \t\t\t{os_name.group(1)}")
     # OS Version
-    os_version = re.search(r'^VERSION="([^"]+)"', os_info)
+    os_version = re.search(r'^VERSION_ID="([^"]+)"', os_info,re.MULTILINE)
     print(f"Operating System: \t\t\t{os_version.group(1)[:4]}")
     # Kernal Version
     kernal = subprocess.run("uname -r",capture_output=True,shell=True).stdout.strip().decode()
     print(f"Kernal Version: \t\t\t{kernal}")
 
 def storage_info():
+    print(f"\033[92mStorage Information\n\033[00m")
     # System Drive Total:
     info = subprocess.run(["df", "-h", "/"], capture_output=True, text=True).stdout
-    match = re.search(r"^/dev/sda1\s+(\S+)\s+(\S+)\s+(\S+)", info, re.MULTILINE)
-    size, used, avail = match.groups()
-    print(f"Size: \t\t\t{size}")
+    match = re.findall(r'(\d+(?:\.\d+)?)G', info)
+    size, used, avail = match[0],match[1],match[2]
+    print(f"Size: \t\t\t{size}GiB")
     # System Drive Used:
-    print(f"Used: \t\t\t{used}")
+    print(f"Used: \t\t\t{used}GiB")
     # System Drive Free:
-    print(f"Avail: \t\t\t{avail}")
-    pass
+    print(f"Avail: \t\t\t{avail}GiB")
 
 def processor_info():
+    print(f"\033[92mProcessor Information\n\033[00m")
+    model = None
+    processors = 0
+    cores = set()
+
+    with open("/proc/cpuinfo") as f:
+        for line in f:
+            if line.startswith("model name") and model is None:
+                model = line.split(":", 1)[1].strip()
+            if line.startswith("processor"):
+                processors += 1
+            if line.startswith("core id"):
+                cores.add(line.split(":", 1)[1].strip())
     # CPU model
-
+    print(f"CPU Model: \t\t\t{model}")
     # Number of Processors 
-
+    print(f"Number of processors: \t\t\t{processors}")
     # Number of Cores
-
-    pass
+    print(f"Number of cores: \t\t\t{len(cores)}")
+    
 
 def memory_info():
+    print(f"\033[92mMemory Information\n\033[00m")
     # Total RAM
-
+    info = subprocess.run("free -h",capture_output=True,shell=True).stdout.strip().decode()
+    match = re.findall(r'(\d+(?:\.\d+)?)Gi', info)
+    total, avail = match[0],match[1]
+    print(f"Used: \t\t\t{total}GiB")
     # Available RAM
-
-    pass
+    print(f"Used: \t\t\t{avail}GiB")
 
 def main():
-    # print("Hello")
-    # device_information()
-    # network_information()
+    device_information()
+    network_information()
+    os_information()
+    storage_info()
     processor_info()
+    memory_info()
 
 os.system('clear')
 main()
